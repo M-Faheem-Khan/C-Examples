7c5862f4f515f138d6330fa79fb38c1bda4eda67
M-Faheem-Khan <faheem5948@gmail.com>
2019-09-03 16:56:36 -0500

    Added Hello World

---
+++ week1/src/hello_world.c
@@ -0,0 +1,14 @@
+/*
+    Name: hello_world.c
+    Author: Muhammad F. Khan
+    Version: 1.0
+    Description: Hello World Program in C Programming Language
+*/
+
+// Imports
+#include <stdio.h> // for input and output operations
+
+// main function
+void main(){
+    printf("Hello World\n"); // writing Hello World to the screen
+}
